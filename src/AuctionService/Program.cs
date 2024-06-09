99b5646d9c5b97cc53f374afedf409d606640905
Amir Zahari <amirzahari91@gmail.com>
2024-06-09 19:36:24 +0800

    end of section 2

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -7,9 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<AuctionDbContext>(opt => {
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -18,5 +21,8 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try { DbInitializer.InitDb(app); }
+catch (Exception ex) { Console.WriteLine(ex); }
 
 app.Run();
---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        // From (Aution + Item) to AuctionDto
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+
+        // from CreateAuctionDto to (Aution + Item)
+        // mapping some source properties to destination Item
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(dest => dest.Item, o => o.MapFrom(src => src));
+        CreateMap<CreateAuctionDto, Item>();
+
+    }
+}
